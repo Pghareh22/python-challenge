c3192acfdb4db8df4641c480b703ba522855e066
Pghareh22 <38015826+Pghareh22@users.noreply.github.com>
2018-08-14 10:51:18 -0700

    update main.py in pypoll

---
+++ PyPoll/main.py
@@ -0,0 +1,45 @@
+import csv , os
+
+path= os.path.join ('..' , 'PyPoll' , 'election_data.csv')
+
+counta=0
+candidate=[]
+unique=[]
+candidate=[]
+count=0
+votes=[]
+
+
+with open (path , "r" , newline='' , encoding="UTF-8" ) as vote_data:
+    csv_reader= csv.reader(vote_data , delimiter= "," )
+
+    header= next(csv_reader)
+    
+    for row in csv_reader:
+        counta+=1
+        candidate.append(row[2])   
+
+    unique.append(candidate[0])
+    for i in candidate:
+        if i not in unique:
+            unique.append(i)
+
+
+    for i in range(len(unique)):
+
+        for name in candidate:
+
+            if name== unique[i]:
+                count+=1  
+        votes.append(count)
+        count=0
+
+  
+
+
+print("Election Results")
+print("----------------------------")
+print (f"Total Votes : {counta}")
+print("----------------------------")
+print (unique)
+print(votes)
